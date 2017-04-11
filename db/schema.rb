ab92b2da154c11664f0344f43dd485d9de55ce97
Brian Arpaio <arpaio@gmail.com>
2017-04-11 15:13:42 +0000

    added user reference to Voters table, is now set as a foreign key and indexed (user_id)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170404233959) do
+ActiveRecord::Schema.define(version: 20170411151050) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -35,6 +35,8 @@ ActiveRecord::Schema.define(version: 20170404233959) do
     t.string   "phone_number"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_voters_on_user_id"
   end
 
 end
